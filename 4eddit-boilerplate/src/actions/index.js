71e48a5b15b0add69d7db7ef0ef2997efd21f65f
Jmsantini <jmsantini@live.com>
2020-01-21 10:36:23 -0300

    LocalStore login/ Post mostrando

--- 4eddit-boilerplate/src/actions/index.js
+++ 4eddit-boilerplate/src/actions/index.js
@@ -12,22 +12,6 @@ const postLogin = (login) => ({
     }
 })
 
-export const postLoginUser = (email, password) => async (dispatch) =>{
-  
-    const newUser = {
-        email,
-        password,
-    }
-
-    try{
-        await axios.post(`${baseUrl}/login`, newUser)
-        dispatch(postLogin())
-        window.alert("Login Realizado com sucessso!!!");
-    }catch(error){
-        window.alert("Login ou senha incorreta!!!")
-    }
-
-}
 
 const setPosts = (posts) => ({
     type: "SET_POSTS",
@@ -36,10 +20,39 @@ const setPosts = (posts) => ({
     }
 })
 
-export const getPosts = () => async (dispatch) => {
-    const response = await axios.get(`${baseUrl}/posts`)
-    
+export const postLoginUser = (email, password) => async (dispatch) =>{
+  
+    const newUser = {
+        email,
+        password,
+    }
 
+    try{
+      const response = await axios.post(`${baseUrl}/login`, newUser)
+            window.localStorage.setItem("token", response.data.token);
+
+        dispatch(postLogin())
+            window.alert("Login Realizado com sucessso!!!");
+    }catch(error){
+        window.alert("Login ou senha incorreta!!!")
+    }
+
+}
+
+
+
+export const getPosts = () => async (dispatch) => {
+    const token = localStorage.getItem("token")
+    try{
+    const response = await axios.get(`${baseUrl}/posts`,{
+        headers: {
+            auth: token,
+        }
+    })
     dispatch(setPosts(response.data.posts))
+}catch(error){
+    window.alert("Erro")
+
+}
 
 }
