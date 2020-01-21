89aaec995b9c2a11b84e04df86c4ea59d5519ccf
Mariana <coelhomariana@outlook.com>
2020-01-21 13:52:27 -0300

    adicionando o cadastro

--- 4eddit-boilerplate/src/reducers/index.js
+++ 4eddit-boilerplate/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import login from "./login";
 import posts from "./posts";
+import users from "./user";
 
 export const generateReducers = history =>
   combineReducers({
     router: connectRouter(history),
     login,
     posts,
+    users,
     // Outros reducers aqui
   });
