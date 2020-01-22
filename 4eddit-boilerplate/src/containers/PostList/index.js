0553921601a015277fd226a4e130bcd621b9136f
Mariana <coelhomariana@outlook.com>
2020-01-22 11:04:43 -0300

    Css do post list

--- 4eddit-boilerplate/src/containers/PostList/index.js
+++ 4eddit-boilerplate/src/containers/PostList/index.js
@@ -2,10 +2,47 @@ import React, {Component} from "react";
 import { routes } from "../Router"
 import { getPosts } from "../../actions"
 import { connect } from "react-redux";
+import styled from "styled-components";
+import ArrowUpwardRoundedIcon from '@material-ui/icons/ArrowUpwardRounded';
+import ArrowDownwardRoundedIcon from '@material-ui/icons/ArrowDownwardRounded';
 
+const PostDiv = styled.div`
+border: 1px solid #cdcdcc;
+padding: 5px;
+margin: 10px auto 10px auto;
+background-color: white;
+max-width: 50%;
+border-radius: 6px;
+font-family: verdana;
+font-size: 11px;
+border-left: 3px solid #c46210;
+`;
 
+const PostContainer = styled.div`
+margin: auto;
+`
 
+const UserName = styled.p`
+border-bottom: 1px solid grey;
+margin-top: 2px;
+`
+const BackgroundDiv = styled.div`
+background-color: #fecbbd;
+display: flex;
+justify-content: center;
+`
 
+const DetailsButton = styled.button`
+background-color: white;
+border: 1px solid grey;
+border-radius: 5px;
+margin: 10px 0 5px 0;
+`
+
+const PostedBy = styled.span`
+color: grey;
+font-size: 9px;
+`
 
 export class PostList extends Component {
     componentDidMount(dispatch) {
@@ -14,21 +51,22 @@ export class PostList extends Component {
 
     render(){
         return(
-            <div>
+            <BackgroundDiv>
                
-                <p>Post List</p>
-
-                <div>
+                <PostContainer>
                     {this.props.posts.map((posts)=>
-                    <p>
+                    <PostDiv>
+                      <UserName><PostedBy>Postado por: </PostedBy>{posts.username}</UserName>
                       {posts.text}  
-                      {posts.username}
-                    </p>
+                      <div>(0) comentários</div> {/* quantidade de comentários vai aqui depois*/}
+                      <p><ArrowUpwardRoundedIcon/><ArrowDownwardRoundedIcon/></p>
+                      <DetailsButton>Detalhes do post</DetailsButton>
+                    </PostDiv>
                     )}
 
-                </div>
+                </PostContainer>
 
-            </div>
+            </BackgroundDiv>
         )
     }
 }
