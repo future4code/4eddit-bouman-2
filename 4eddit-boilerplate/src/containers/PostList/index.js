6b6bfc041f8090d482bcb16224086f56d6832b87
unknown <eduardomendesnorberto@outlook.com>
2020-01-22 15:06:01 -0300

    Post detalhes implementado

--- 4eddit-boilerplate/src/containers/PostList/index.js
+++ 4eddit-boilerplate/src/containers/PostList/index.js
@@ -1,18 +1,36 @@
 import React, {Component} from "react";
-import { routes } from "../Router"
-import { getPosts } from "../../actions"
+import { routes } from "../Router";
+import { getPosts } from "../../actions";
 import { connect } from "react-redux";
+import styled from "styled-components";
+import { push } from "connected-react-router";
+import { getPostDetail } from "../../actions";
 
+const CardPost = styled.div `
+    border: 1px dotted black;
+    max-width: 300px;
+    margin-bottom: 20px;
+`;
 
 
 
 
 export class PostList extends Component {
+    
+    
     componentDidMount(dispatch) {
-        this.props.getPosts()
+        this.props.getPosts(this.props.id)
+    }
+
+
+    handleIdPostAndGoToPostDetails = (postId) =>{
+        this.props.getPostId(postId);
+        this.props.goToPostDetails();
     }
 
     render(){
+        const { getPostId } = this.props;
+        
         return(
             <div>
                
@@ -20,10 +38,12 @@ export class PostList extends Component {
 
                 <div>
                     {this.props.posts.map((posts)=>
-                    <p>
-                      {posts.text}  
-                      {posts.username}
-                    </p>
+                        <CardPost>  
+                            <p>{posts.title}</p>
+                            <p>{posts.id}</p>
+                            <p>postado por: {posts.username}</p>
+                            <p onClick={() => this.handleIdPostAndGoToPostDetails(posts.id)}>{posts.text}</p>  
+                        </CardPost>
                     )}
 
                 </div>
@@ -35,10 +55,13 @@ export class PostList extends Component {
 
 const mapStateToProps = state => ({
     posts: state.posts.allPosts,
+    idSelectedPost: state.posts.postId,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    getPosts: () => dispatch(getPosts())
+    getPosts: () => dispatch(getPosts()),
+    goToPostDetails: ()=> dispatch(push(routes.postdetails)),
+    getPostId: (postId)=> dispatch(getPostDetail(postId)),
 
 })
 
